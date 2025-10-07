5ed2da54b72b43c46f5908b8249f0380148ed354
paralay <160874240+paralay@users.noreply.github.com>
2025-10-07 11:32:23 +0200

    Player color and nametag

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,8 @@
+using System.Drawing;
+using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : NetworkBehaviour
 {
@@ -7,10 +10,21 @@ public class Player : NetworkBehaviour
 
     [SerializeField] private Material mat;
     [SerializeField] private Camera playerCamera;
+    [SerializeField] private Text nameTag;
+
+    public NetworkVariable<FixedString32Bytes> PlayerName = new NetworkVariable<FixedString32Bytes>(
+    default,
+    NetworkVariableReadPermission.Everyone,
+    NetworkVariableWritePermission.Owner);
+
+    public NetworkVariable<Color32> PlayerColor = new NetworkVariable<Color32>(
+        default,
+        NetworkVariableReadPermission.Everyone,
+        NetworkVariableWritePermission.Owner);
 
     void Start()
     {
-        if(IsOwner) GetComponent<MeshRenderer>().material = mat;
+
     }
     void Update()
     {
@@ -25,11 +39,31 @@ public class Player : NetworkBehaviour
     {
         if (IsOwner)
         {
+            PlayerColor.Value = Random.ColorHSV();
+
+            string localName = PlayerPrefs.GetString("PlayerName", $"Player{OwnerClientId}");
+            PlayerName.Value = localName;
             playerCamera.gameObject.SetActive(true);
         }
         else
         {
             playerCamera.gameObject.SetActive(false);
         }
+
+        GetComponent<MeshRenderer>().material = new Material(mat);
+        GetComponent<MeshRenderer>().material.color = PlayerColor.Value;
+
+        nameTag.text = PlayerName.Value.ToString();
+
+        PlayerName.OnValueChanged += (oldName, newName) => {
+                nameTag.text = newName.ToString();
+        };
+
+        PlayerColor.OnValueChanged += (oldColor, newColor) => {
+                GetComponent<MeshRenderer>().material.color = newColor;
+        };
+
+
+        if (IsOwner) { nameTag.text += " (you)"; }
     }
 }

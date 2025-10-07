fa16c007c48d56f50fbd438fa106a07bec721c8a
paralay <160874240+paralay@users.noreply.github.com>
2025-10-07 14:16:29 +0200

    Camera fix + tutorial

--- Assets/cameracontrol.cs
+++ Assets/cameracontrol.cs
@@ -3,18 +3,36 @@ using UnityEngine;
 public class cameracontrol : MonoBehaviour
 {
     float xRotation = 0;
-    float sensitivity = 100f;
+    float sensitivity = 300f;
 
     [SerializeField] GameObject parent;
+
+    bool isActive = false;
     void Start()
     {
+
         xRotation = transform.eulerAngles.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isActive = !isActive;
+            if (isActive)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+        }
+
+        if (isActive)
         {
             float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
             float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
